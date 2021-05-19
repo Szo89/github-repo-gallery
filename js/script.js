636e8f93f5d781587b92b8ecea57a3ee852377a1
Szo89 <susanazoliveira@gmail.com>
2021-05-19 11:12:50 +0100

    adding repos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const overwiew = document.querySelector(".overview") //profile information
-let username = "Szo89";
+const repoList = document.querySelector(".repo-list");
+const username = "Szo89";
+
 
 const getUserInfo = async function(){
     const userInfo = await fetch(`https://api.github.com/users/${username}`);    
@@ -25,4 +27,22 @@ const displayUserInfo = function(data){
         </div>
    `
    overwiew.append(div)
+}
+
+const userRepos = async function(){
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=10`);
+    const repoData = await fetchRepos.json();
+    console.log(repoData);
+    displayRepo(repoData);
+}
+
+userRepos();
+
+const displayRepo = function(repoData){
+    for (const repo of repoData){
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerText = `${repo.name}`;
+        repoList.appendChild(li)
+    }
 }
